cc714ccf86ce695de2bea69d6b7cd74d84be95d3
KKK <KKK@DESKTOP-OERHPBH>
2018-06-26 17:26:28 +0900

    ...

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,27 +1,41 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.dao.UserRepository;
 import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
 
 @RestController
 public class UserController {
+	
 	@Autowired
 	private UserRepository userRepository;
-
+	
+	@Autowired
+	private UserService userService;
+	
+	/*
+	@Autowired
+	private UserRepository2 userRepository2;
+*/
 	/**
-	 * http://localhost:8080/add?name=김형진&phone=01076041440
+	 * http://localhost:8080/add?name=源��삎吏�&phone=01076041440
 	 * @param user
 	 * @return
 	 */
-	@RequestMapping("/add")
+	@RequestMapping(path = "/add", method = RequestMethod.GET)
 	public User add(User user) {
 
 		User userResult = userRepository.save(user);
@@ -49,20 +63,70 @@ public class UserController {
 
 		return "User Delete!";
 	}
-	
+
 	@RequestMapping("/update")
 	public User update(User user) {
 		
-		 User userResult = userRepository.findById(user.getId()).get();
+		 User userResult = userRepository.findById(user.getUserId()).get();
 		 userResult.setName(user.getName());
 		 userResult.setPhone(user.getPhone());
 		 userRepository.save(userResult);
 		
 		return userResult;
 	}
-
+	
 	@RequestMapping("/")
 	public String index() {
 		return "User JPA Test!";
+	}
+	
+	@PersistenceContext 
+	EntityManager em;
+	
+	@RequestMapping("/jpaTest")
+	public Page<User> jpaTest(User user, Pageable pageRequest) {
+		
+		
+		//http://localhost:8080/jpaTest
+		//http://localhost:8080/jpaTest?userId=1
+		Page<User> userPage = userService.search(user.getUserId(), user.getName(), pageRequest);
+		
+		/*
+		
+		switch (user.getTestType()) {
+		case "0" :
+			//userList = userRepository2.findByName(user.getName());
+			break;
+		case "1" :
+			//userList = userRepository2.findByPhone(user.getPhone());
+			break;
+		case "2" :
+			
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("user");
+			EntityManager em = emf.createEntityManager();
+			EntityTransaction tx = em.getTransaction();
+			try {
+				tx.begin();
+				tx.commit();
+			}catch(Exception e) {
+				e.printStackTrace();
+				tx.rollback();
+			}finally {
+				em.close();
+			}
+			emf.close();
+			
+			TypedQuery<User> query = em.createNamedQuery("User.findAll", User.class);
+			userList = query.getResultList();
+			break;
+		case "3" :
+			//userList = userRepository2.findByName(user.getName());
+			break;
+		default:
+			break;
+		}*/
+		
+		
+		return userPage;
 	}
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -5,43 +5,49 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  *
  */
 @Entity
-public class User {
+@ToString
+@Setter
+@Getter
+//@Data //Getter, Setter, ToString, EqualsAndHashCode 모두 구현되는 것 같음.
+@Table(name = "user", schema = "")
+//@NamedQuery(name = "User.findByEmailAddress", query = "select u from User u where u.emailAddress = ?1")
+/*@NamedQueries({
+    @NamedQuery(name="User.findAll",
+                query="SELECT u FROM User u"),
+    @NamedQuery(name="User.findByName",
+                query="SELECT u FROM User u WHERE u.name = :name"),
+    @NamedQuery(name="User.findByPhone",
+    query="SELECT u FROM User u WHERE u.phone = ?1"),
+})*/ 
+public class User{
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@Column(name = "userId", updatable = false, nullable = false)
+	private Long userId;
 
 	@Column(length = 20, nullable = false)
 	private String name;
 
 	@Column(length = 20, nullable = false, unique = true)
 	private String phone;
+	
+	private String testType;
 
-	public Long getId() {
-		return id;
-	}
+	// @OneToOne
+	// @JoinColumn(name = "userId")
+	// private Detail detail;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
 }
